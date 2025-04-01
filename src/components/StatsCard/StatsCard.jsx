1d911d083d19383d5cf507128bfd26cc7b92054f
OrhanS05 <orhan.sevilir@hotmail.com>
2025-04-01 14:47:48 +0200

    added images to dashboard components (water, steps and weight)

--- src/components/StatsCard/StatsCard.jsx
+++ src/components/StatsCard/StatsCard.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import "./StatsCard.css";
 import Button from "../Button/Button";
 import LinearProgress from "../LinearProgress/LinearProgress";
+import stepImage from "../../assets/dashboard/steps.png";
+import waterImage from "../../assets/dashboard/water.png";
 
 function StatsCard({ title, consumed, goal, onEdit }) {
   const progressColor = title === "Stappen" ? "red" : "blue";
+  const imageSrc = title === "Stappen" ? stepImage : waterImage;
 
   return (
     <div className="stats-card">
       <div className="stats-card-header">
+        <img src={imageSrc} alt={title} className="stats-card-image" />
         <h3>{title}</h3>
         <Button text="Bewerken" className="blue" onClick={onEdit} />
       </div>
